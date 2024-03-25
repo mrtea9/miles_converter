96ba13dfa4545acb48fa3363fbe56c51342f1f94
mrtea9 <ionmeriacri111@gmail.com>
2024-03-25 13:35:49 +0200

    Miles to Kilometer Converter

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ is_equal_label.grid(column=0, row=1)
 miles_label = Label(text="Miles")
 miles_label.grid(column=2, row=0)
 
-
 km_label = Label(text="km")
 km_label.grid(column=2, row=1)
 
@@ -29,9 +28,8 @@ km_result_label.grid(column=1, row=1)
 calculate_button = Button(text="Calculate", command=miles_to_km)
 calculate_button.grid(column=1, row=2)
 
-
 miles_entry = Entry(width=10)
 miles_entry.grid(column=1, row=0)
 
-window.mainloop()
 
+window.mainloop()
